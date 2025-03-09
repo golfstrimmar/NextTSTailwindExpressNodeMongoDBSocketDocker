53ee2365c1db26cfbb3f6af32432c42ea32e5b1e
golfstrimmar <yushinbox@gmail.com>
2025-03-09 09:36:17 +0100

    qqq

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,30 +13,30 @@ interface Auction {
 const HomePage = () => {
   const [auctions, setAuctions] = useState<Auction[]>([]);
 
-  useEffect(() => {
-    const fetchAuctions = async () => {
-      try {
-        const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-        const response = await axios.get(apiUrl!);
-        setAuctions(response.data);
-      } catch (error) {
-        console.error("Error fetching auctions:", error);
-      }
-    };
-    fetchAuctions();
-  }, []);
+  // useEffect(() => {
+  //   const fetchAuctions = async () => {
+  //     try {
+  //       const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  //       const response = await axios.get(apiUrl!);
+  //       setAuctions(response.data);
+  //     } catch (error) {
+  //       console.error("Error fetching auctions:", error);
+  //     }
+  //   };
+  //   fetchAuctions();
+  // }, []);
 
   return (
     <div>
       <h1 className="text-3xl font-bold text-center my-4">Auction Home</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {auctions.map((auction) => (
           <div key={auction.id} className="border p-4 rounded-lg">
             <h2 className="text-xl font-semibold">{auction.title}</h2>
             <p>Current Bid: ${auction.currentBid}</p>
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
